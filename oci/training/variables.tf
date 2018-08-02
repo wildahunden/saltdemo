fed5d22b5e9c743c7e0b459fa6ffee8f0de3dd1a
Daniel Williams <wildahunden@gmail.com>
2018-08-02 13:29:21 -0500

    save training files

---
+++ oci/training/variables.tf
@@ -0,0 +1,13 @@
+
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+
+variable "compartment_ocid" {}
+
+variable "VPC-CIDR" {
+  default = "10.0.0.0/16"
+}
+
