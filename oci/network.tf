e68e826466c61a1f693cef795d0dcfda7a04fd5d
Daniel Williams <wildahunden@gmail.com>
2018-08-02 14:02:35 -0500

    vcn and subnets for webservers working

--- oci/network.tf
+++ oci/network.tf
@@ -1,3 +1,6 @@
+#Create the VCN that will house this demo
+#  2 load balancers in 2 public subnets
+#  2 web servers in 2 public subnets
 resource "oci_core_virtual_network" "saltdemo-VCN" {
   cidr_block     = "${var.VPC-CIDR}"
   compartment_id = "${var.compartment_ocid}"
@@ -5,8 +8,21 @@ resource "oci_core_virtual_network" "saltdemo-VCN" {
   dns_label      = "saltdemovcn"
 }
 
+resource "oci_core_subnet" "saltdemo-subnet-ws1" {
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id         = "${oci_core_virtual_network.saltdemo-VCN.id}"
+  "availability_domain" = "aNUQ:US-ASHBURN-AD-1"
+  "cidr_block"   = "10.0.0.0/24"
+}
 
-resource="oci_core_internet_gateway" "saltdemo-IGW" {
+resource "oci_core_subnet" "saltdemo-subnet-ws2" {
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id         = "${oci_core_virtual_network.saltdemo-VCN.id}"
+  "availability_domain" = "aNUQ:US-ASHBURN-AD-2"
+  "cidr_block"   = "10.0.1.0/24"
+}
+
+resource "oci_core_internet_gateway" "saltdemo-IGW" {
   compartment_id = "${var.compartment_ocid}"
   display_name   = "saltdemo-IGW"
   vcn_id         = "${oci_core_virtual_network.saltdemo-VCN.id}"
--- oci/variables.tf
+++ oci/variables.tf
@@ -5,8 +5,7 @@ variable "fingerprint" {}
 variable "private_key_path" {}
 variable "region" {}
 
-variable "compartment_ocid" {}
-
+variable "compartment_ocid" { } 
 variable "VPC-CIDR" {
   default = "10.0.0.0/16"
 }
